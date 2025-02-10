1ef1f5bc58254c95d87ec3f9163abfb5243eb244
Chen Nuo <49788094+Cstandardlib@users.noreply.github.com>
2025-02-10 13:01:16 +0800

    Update calculations.py to support aiida-pseudo

--- src/aiida_abacus/calculations.py
+++ src/aiida_abacus/calculations.py
@@ -110,7 +110,11 @@ class DiffCalculation(CalcJob):
 
         self.write_input(INPUT)
         self.write_kpoints(KPT)
+        print("self.inputs.structure is: ", self.inputs.structure)
+        print("self.inputs.structure.kinds is: ", self.inputs.structure.kinds)
+        self.local_pseudo_copy_list = [] # will be written in generate_structure inside write_stru
         self.write_stru(STRU)
+        print("self.local_pseudo_copy_list is:", self.local_pseudo_copy_list)
 
         codeinfo = datastructures.CodeInfo()
         # codeinfo.cmdline_params = self.inputs.parameters.cmdline_params(
@@ -120,11 +124,14 @@ class DiffCalculation(CalcJob):
         # no cmdline params needed
         codeinfo.cmdline_params = []
         codeinfo.code_uuid = self.inputs.code.uuid
-        codeinfo.stdout_name = self.metadata.options.output_filename
+        # codeinfo.stdout_name = self.metadata.options.output_filename
 
         # Prepare a `CalcInfo` to be returned to the engine
         calcinfo = datastructures.CalcInfo()
         calcinfo.codes_info = [codeinfo]
+        local_copy_list = []
+        local_copy_list.extend(self.local_pseudo_copy_list)
+        calcinfo.local_copy_list = local_copy_list
         # calcinfo.local_copy_list = [
         #     (
         #         self.inputs.file1.uuid,
@@ -140,6 +147,8 @@ class DiffCalculation(CalcJob):
 
         # retrieve the output folder OUT.aiida
         calcinfo.retrieve_list = [self._OUTPUT_SUBFOLDER]
+        # print("calcinfo is:", calcinfo)
+        print("retrieve:", calcinfo.retrieve_list)
 
         return calcinfo
     
@@ -216,8 +225,10 @@ class DiffCalculation(CalcJob):
         see the `ABACUS Input Guide <https://abacus.deepmodeling.com/en/latest/advanced/input_files/stru.html>`_.
         :param structure: a StructureData object
         :param pseudos: a dictionary of pseudopotential nodes
-        :return: the content of the input file STRU"""
+        :return: the content of the input file STRU & a list of pseudopotential files to be copied"""
         # may add some validation here, and maybe some conversions
+        print("structure is:", structure)
+        print("structure.kinds is:", structure.kinds)
 
         # This is the atom file containing all the information about the lattice structure.
         structure_list = []
@@ -234,13 +245,15 @@ class DiffCalculation(CalcJob):
         # ATOMIC_SPECIES section
         # This section provides information about the type of chemical elements contained the unit cell. 
         # structure_list.append("ATOMIC_SPECIES\n")
-        atomic_species = ["ATOMIC_SPECIES\n"]
+        atomic_species = ["ATOMIC_SPECIES"]
         # for species in structure_data["atomic_species"]:
         #     structure_list.append(f"{species['label']} {species['mass']} {species['pseudo_file']} {species['pseudo_type']}")
         # I keep track of the order of species
         kind_names = []
         # I add the pseudopotential files to the list of files to be copied
         for kind in structure.kinds:
+            print("kind is:", kind)
+            print("kind.name is:", kind.name)
             # This should not give errors, I already checked before that
             # the list of keys of pseudos and kinds coincides
             pseudo = pseudos[kind.name]
@@ -261,7 +274,8 @@ class DiffCalculation(CalcJob):
                 )
 
             kind_names.append(kind.name)
-            atomic_species.append(f'{kind.name.ljust(6)} {kind.mass} {filename}\n')
+            atomic_species.append(f'{kind.name.ljust(6)} {kind.mass}   {filename}')
+        print("atomic_species is:", atomic_species)
 
         structure_list.extend(atomic_species)
 
@@ -277,31 +291,104 @@ class DiffCalculation(CalcJob):
 
         # LATTICE_CONSTANT section
         # The lattice constant of the system in unit of Bohr.
-        # structure_list.append("\nLATTICE_CONSTANT\n")
-        # structure_list.append(f"{structure['lattice_constant']}")
+        lattice_constant = ["\nLATTICE_CONSTANT"]
+        print("structure.cell is:", structure.cell)
+        lattice_const_in_bohr = 1.8897259886 		# 1.8897259886 Bohr =  1.0 Angstrom
+        lattice_constant.append(str(lattice_const_in_bohr))
+        structure_list.extend(lattice_constant)
 
         # LATTICE_VECTORS section
         # This section is only relevant when latname (see input parameters) is used to specify the Bravais lattice type.
         # structure_list.append("\nLATTICE_VECTORS\n")
         # for vector in structure["lattice_vectors"]:
         #     structure_list.append(" ".join(map(str, vector)))
-        lattice_vectors = ["LATTICE_VECTORS\n"]
+        lattice_vectors = ["\nLATTICE_VECTORS"]
         for vector in structure.cell:
             lattice_vectors.extend([f"{vector[0]:20}{vector[1]:20}{vector[2]:20}"])
+        structure_list.extend(lattice_vectors)
 
         # ATOMIC_POSITIONS section
         # This section specifies the positions and other information of individual atoms.
-        atom_positions = ["ATOMIC_POSITIONS\n"]
-        structure_list.append("\nATOMIC_POSITIONS\n")
-        structure_list.append(structure["atomic_positions"]["coordinate_type"])
-        for element, magnetism, count, positions in structure["atomic_positions"]["atoms"]:
-            structure_list.append(f"{element} {magnetism} {count}")
-            for pos in positions:
-                structure_list.append(" ".join(map(str, pos)))
+        atom_positions = [
+            "ATOMIC_POSITIONS",
+            "Direct", # The first line signifies method that atom positions are given, e.g. Cartesian or Direct.
+            # next lines are atom-specific information, processed in the following code
+        ]
+
+        # structure_list.append("\nATOMIC_POSITIONS\n")
+        # structure_list.append(structure["atomic_positions"]["coordinate_type"])
+        # for element, magnetism, count, positions in structure["atomic_positions"]["atoms"]:
+        #     structure_list.append(f"{element} {magnetism} {count}")
+        #     for pos in positions:
+        #         structure_list.append(" ".join(map(str, pos)))
+        # atom position dict consists of 4 parts:
+        # part 1 Element type
+        # part 2 magnetism (Be careful: value 1.0 refers to 1.0 bohr mag, but not fully spin up !!!)
+        # part 3 number of atoms
+        # part 4 the position of atoms and other parameter specify by key word
+        # 4 details see https://abacus.deepmodeling.com/en/latest/advanced/input_files/stru.html#atomic-positions
+        # e.g. keyword `m` or no keywords - followed by move_x, move_y, move_z
+        # x,y,z, [m,] move_x, move_y, move_z
+        # the numbers 0 0 0 (0 or 1 each for false and true) following the coordinates of the atom
+        #   means this atom is allowed or not to move in the three directions three numbers,
+        #   which take value in 0 or 1, control how the atom moved in geometry relaxation calculations
+        
+
+        # construct atom position dict
+        atom_position_dict = {}
+        # each key-value pair: key is the kind name, value is a list of atom positions and other parameters
+# this should be given as inputs!
+        # keyword m: the atom is allowed to move in geometry relaxation calculations
+        move_list = [0, 0, 0] # default value for move_x, move_y, move_z
+        coordinates = [site.position for site in structure.sites]
+        print("sites is:", structure.sites)
+        print("coordinates is:", coordinates)
+        # Add and count atoms.
+        # The following three lines tells the elemental type (Fe),
+        # the initial magnetic moment (1.0),
+        # and the number of atoms for this particular element (2) repsectively.
+        for site, site_coords in zip(structure.sites, coordinates):
+            kind_name = site.kind_name
+            position = [
+                *site_coords,
+                'm', # m or NO key word: three numbers, which take value in 0 or 1,
+                     # control how the atom move in geometry relaxation calculations. 
+                *move_list
+            ]
+            if kind_name not in atom_position_dict:
+                atom_position_dict[kind_name] = {
+                    "number_of_atoms": 1,
+# need to add magnetism into readin parameters
+                    "initial_magnetic_moment": 1.0,
+                    "positions": []
+                }
+            else:
+                atom_position_dict[kind_name]["number_of_atoms"] += 1
+            atom_position_dict[kind_name]["positions"].append(position)
+
+        print("atom_position_dict is:", atom_position_dict)
+
+        # write atom_position_dict into atom_positions
+        for kind_name, kind_dict in atom_position_dict.items():
+            atom_positions.append(
+                f"{kind_name}\n{kind_dict["initial_magnetic_moment"]}\n{kind_dict['number_of_atoms']}"
+            )
+            for position in kind_dict["positions"]:
+                atom_positions.append(" ".join(map(str, position)))
+        
+        print("atom_positions is:", atom_positions)
+
+        # atom_positions += [
+        #     '{0} {1:18.10f} {2:18.10f} {3:18.10f}'.format(site.kind_name.ljust(6), *site_coords)  # pylint: disable=consider-using-f-string
+        #     for site, site_coords in zip(structure.sites, coordinates)
+        # ]
+        # atom_positions.append("\n")
+        structure_list.extend(atom_positions)
 
         # Join the structure_list into a single string
+        print("structure_list is:", structure_list)
         structure_content = "\n".join(structure_list)
-        return structure_content
+        return structure_content, local_copy_list_to_append
 
         # for site in structure.sites:
         #     # The longest parameter is 'bessel_descriptor_tolerence' with 27 characters.
@@ -311,7 +398,11 @@ class DiffCalculation(CalcJob):
     
     def write_stru(self, stru_file):
         """Write the structure file STRU."""
-        structure_content = self.generate_structure(self.inputs.structure, self.inputs.pseudos)
+        print("self.inputs.structure is: ", self.inputs.structure)
+        print("self.inputs.structure.kinds is: ", self.inputs.structure.kinds)
+        structure_content, local_pseudo_copy_list = self.generate_structure(self.inputs.structure, self.inputs.pseudos)
+        self.local_pseudo_copy_list = local_pseudo_copy_list
+        # structure_content = self.generate_structure(self.inputs.structure)
         with open(stru_file, "w") as handle:
             handle.write(structure_content)
     
