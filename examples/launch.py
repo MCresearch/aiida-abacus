7cf7f603be1ba889c9081619e43247c55a07bcd6
Chen Nuo <49788094+Cstandardlib@users.noreply.github.com>
2025-02-12 10:11:11 +0800

    Update launch script to add example info

--- examples/launch.py
+++ examples/launch.py
@@ -1,4 +1,5 @@
-"""Launch a calculation using the 'diff-tutorial' plugin"""
+"""Launch a calculation using the 'aiida-abacus' plugin"""
+# We will use the abacus-develop/examples/scf/pw_Si2 directory as an example
 
 from pathlib import Path
 
@@ -15,8 +16,8 @@ import numpy as np
 # parameters = DiffParameters({"ignore-case": True})
 # parameters = {"ignore-case": True}
 
-
-# Create or load code
+###
+# set up code
 computer = orm.load_computer('localhost')
 # try:
 #     code = orm.load_code('diff@localhost')
@@ -33,10 +34,81 @@ code = orm.InstalledCode(
     default_calc_job_plugin='abacus'
 )
 
-# Set up inputs
+
 builder = code.get_builder()
 
+builder.metadata.options = {
+    'resources': {
+        'num_machines': 1,
+        "num_mpiprocs_per_machine": 1, # use 1 cores per machine
+    },
+    'max_wallclock_seconds': 180,
+    # 'withmpi': False, # Set withmpi to False in case abacus was compiled without MPI support.
+}
 
+###
+# set up inputs
+"""
+INPUT_PARAMETERS
+#Parameters  (General)
+pseudo_dir      ./pseudo/
+symmetry        1
+#Parameters  (Accuracy)
+basis_type      pw
+ecutwfc         60  ###Energy cutoff needs to be tested to ensure your calculation is reliable.[1]
+scf_thr         1e-7
+scf_nmax        100
+device          cpu
+ks_solver       dav_subspace
+precision       double
+
+### [1] Energy cutoff determines the quality of numerical quadratures in your calculations.
+###     So it is strongly recommended to test whether your result (such as converged SCF energies) is
+###     converged with respect to the energy cutoff.
+"""
+input_parameters = {
+    # pseudo_dir will be set by the plugin based on the pseudos
+    # 'symmetry': 1,
+    # 'basis_type': 'pw',
+    'ecutwfc': 100,
+    'scf_thr': 1e-4, #1e-7,
+    # 'scf_nmax': 100,
+    # 'device': 'cpu',
+    # 'ks_solver': 'dav_subspace',
+    # 'precision': 'double',
+    # STRU
+    # 'lattice_constant': 10.2,
+}
+# revert input_parameters
+# input_parameters = reversed(input_parameters)
+parameters=Dict(dict=input_parameters)
+
+
+###
+# set up structure
+"""
+#This is the atom file containing all the information
+#about the lattice structure.
+
+ATOMIC_SPECIES
+Si 1.000 Si.pz-vbc.UPF 	#Element, Mass, Pseudopotential
+
+LATTICE_CONSTANT
+10.2  			#Lattice constant
+
+LATTICE_VECTORS
+0.5 0.5 0.0 		#Lattice vector 1
+0.5 0.0 0.5 		#Lattice vector 2
+0.0 0.5 0.5 		#Lattice vector 3
+
+ATOMIC_POSITIONS
+Cartesian 		#Cartesian(Unit is LATTICE_CONSTANT)
+Si 			#Name of element
+0.0			#Magnetic for this element.
+2			#Number of atoms
+0.00 0.00 0.00 0 0 0	#x,y,z, move_x, move_y, move_z
+0.25 0.25 0.25 1 1 1
+"""
 # STRU
 # adadpted from aiida-vasp
 # StructureData = DataFactory('core.structure')
@@ -49,33 +121,28 @@ lattice = [[a, 0, 0], [-a / 2, a / 2 * np.sqrt(3), 0], [0, 0, c]]
 # structure = StructureData(cell=lattice)
 from ase.build import bulk
 structure = StructureData(ase=bulk('Si', 'fcc', 5.43))
-print(structure.sites)
-for site in structure.sites:
-    print(site.kind_name)
-    print(site.position)
+
 
 # KPT
 # KpointsData = DataFactory('core.array.kpoints')
 # The entry point `array.kpoints` is deprecated.
 # Please replace it with `core.array.kpoints`. (this will be removed in v3)
 kpoints = KpointsData()
-kpoints.set_kpoints_mesh([6, 6, 4], offset=[0, 0, 0.5])
+kpoints.set_kpoints_mesh([6, 6, 4], offset=[0, 0, 0.5]) # default cartesian=False
+#! note that according to aiida.orm.nodes.data.array.kpoints.KpointsData:
+# Internally, all k-points are defined in terms of crystal (fractional) coordinates.
+# Cell and lattice vector coordinates are in Angstroms, reciprocal lattice vectors in Angstrom^-1 .
 
-
+###
+# prepare pseudos with aiida-pseudo
 pseudo_family = load_group('SSSP/1.1/PBE/efficiency')
 builder.pseudos = pseudo_family.get_pseudos(structure=structure)
 
 
 builder.structure = structure
-print(builder.structure.get_kind_names())
 builder.kpoints = kpoints
 builder.metadata.description = 'Test job submission with the aiida_abacus plugin'
 
-input_parameters = {
-    'ecutwfc': 100,
-    'scf_thr': 1e-4,
-}
-parameters=Dict(dict=input_parameters)
 
 # Run the calculation & parse results
 results, node = engine.run.get_node(builder, parameters=parameters)
