a74c1c2fd1426a448c41c77899fad94496b52258
Chen Nuo <49788094+Cstandardlib@users.noreply.github.com>
2025-08-19 12:23:01 +0800

    Update example pw si2 demo

--- examples/example_pw_Si2.py
+++ examples/example_pw_Si2.py
@@ -1,23 +1,39 @@
 """Launch a calculation using the 'aiida-abacus' plugin"""
 # We will use the abacus-develop/examples/scf/pw_Si2 directory as an example
 # different pseudos are referenced here!
+# We will
+# 1. set input parameters
+# 2. generate structure
+# 3. generate kpoints
 
 import numpy as np
 from aiida import engine, orm
+from aiida.common.exceptions import NotExistent
 from aiida.orm import Dict, KpointsData, StructureData, load_group
 
 ###
-# set up code
+# set up computer
+# You can easily switch to other computational resources you have
+# including workstation, clusters, etc.
+
 computer = orm.load_computer("localhost")
 
 
-code = orm.InstalledCode(
-    label="abacus", computer=computer, filepath_executable="abacus", default_calc_job_plugin="abacus.abacus"
-)
+# It is possible to configure different versions of code on different computer
+# and the computational results will keep this code info
+try:
+    code = orm.load_code("abacus@localhost")
+except NotExistent:
+    # Setting up code via python API (or use "verdi code setup") if not already set up
+    code = orm.InstalledCode(
+        label="abacus", computer=computer, filepath_executable="abacus", default_calc_job_plugin="abacus.abacus"
+    )
 
-
+# We will next construct the builder
 builder = code.get_builder()
 
+# This is metadata for running the calculation, including
+# the resources to be used, etc.
 builder.metadata.options = {
     "resources": {
         "num_machines": 1,
@@ -29,6 +45,7 @@ builder.metadata.options = {
 
 ###
 # set up inputs
+# this is the INPUT intended
 """
 INPUT_PARAMETERS
 #Parameters  (General)
@@ -42,13 +59,9 @@ scf_nmax        100
 device          cpu
 ks_solver       dav_subspace
 precision       double
-
-### [1] Energy cutoff determines the quality of numerical quadratures in your calculations.
-###     So it is strongly recommended to test whether your result (such as converged SCF energies) is
-###     converged with respect to the energy cutoff.
 """
 
-
+# The INPUT above can be written into Python dictionaries as follows:
 input_parameters = {
     # pseudo_dir will be set by the plugin based on the pseudos
     "symmetry": 1,
@@ -87,67 +100,31 @@ Si 			#Name of element
 0.00 0.00 0.00 0 0 0	#x,y,z, move_x, move_y, move_z
 0.25 0.25 0.25 1 1 1
 """
-# structure_data = {
-#     "atomic_species": [
-#         {"label": "Si", "mass": 28.00, "pseudo_file": "Si_ONCV_PBE-1.0.upf", "pseudo_type": "upf201"}
-#     ],
-#     "numerical_orbital": [
-#         "Si_gga_8au_60Ry_2s2p1d.orb"
-#     ],
-#     "lattice_constant": 10.2,
-#     "lattice_vectors": [
-#         [0.5, 0.5, 0.0],
-#         [0.5, 0.0, 0.5],
-#         [0.0, 0.5, 0.5]
-#     ],
-#     "atomic_positions": {
-#         "coordinate_type": "Direct",
-#         "atoms": [
-#             ("Si", 0.0, 2, [
-#                 [0.00, 0.00, 0.00, 0, 0, 0],
-#                 [0.25, 0.25, 0.25, 1, 1, 1]
-#             ])
-#         ]
-#     }
-# }
 
-
-# STRU
-
-a = 3.092
-c = 5.073
-lattice = [[a, 0, 0], [-a / 2, a / 2 * np.sqrt(3), 0], [0, 0, c]]
-lattice_constant = 10.2  # Å
-
+# STRUCTURE
 lattice_vectors_fractional = np.array([[0.0, 0.5, 0.5], [0.5, 0.0, 0.5], [0.5, 0.5, 0.0]])
 atomic_positions_fractional = [[0.00, 0.00, 0.00], [0.25, 0.25, 0.25]]
-# 构建StructureData
+# create StructureData
 structure = StructureData(cell=lattice_vectors_fractional)
 for pos in atomic_positions_fractional:
     structure.append_atom(position=pos, symbols="Si")
-
-# from ase.build import bulk
-# structure = StructureData(ase=bulk('Si', 'fcc', 5.43))
 
 # structure parameters
 stru_settings = {
     "LATTICE_CONSTANT": 10.2,
     # KEYWORD m : whether or not allowed to move in geometry relaxation calculations.
-    # three numbers, which take value in 0 or 1, control how the atom move in geometry relaxation calculations.
-    # "m": [[False, False, False], [True, True, True]],
+    "m": [[False, False, False], [True, True, True]],  # default set to True
     # KEYWORD mag or magmom : set the start magnetization for each atom.
-    # In colinear case only one number should be given.
-    # In non-colinear case set three number for the xyz commponent of magnetization here (e. g. mag 0.0 0.0 1.0).
-    # Note that if this parameter is set, the initial magnetic moment setting will be overrided.
     # "mag": [
     #     [0.0, 0.0, 0.0]
     # ],
 }
 
+# note ase API is supported by AiiDA and can be used to create structures
+# from ase.build import bulk
+# structure = StructureData(ase=bulk('Si', 'fcc', 5.43))
+
 # KPT
-# KpointsData = DataFactory('core.array.kpoints')
-# The entry point `array.kpoints` is deprecated.
-# Please replace it with `core.array.kpoints`. (this will be removed in v3)
 kpoints = KpointsData()
 kpoints.set_kpoints_mesh([4, 4, 4], offset=[0, 0, 0])  # default cartesian=False
 #! note that according to aiida.orm.nodes.data.array.kpoints.KpointsData:
@@ -159,7 +136,7 @@ kpoints.set_kpoints_mesh([4, 4, 4], offset=[0, 0, 0])  # default cartesian=False
 pseudo_family = load_group("PseudoDojo/0.4/PBE/SR/standard/upf")
 builder.pseudos = pseudo_family.get_pseudos(structure=structure)
 
-
+# parameters dict used to genereate INPUT & STRU files along with structure itself
 all_parameters = {
     "input": input_parameters,
     "stru": stru_settings,
@@ -168,16 +145,21 @@ all_parameters = {
 builder.structure = structure
 builder.kpoints = kpoints
 builder.settings = stru_settings
-builder.metadata.description = "Test job submission with the aiida_abacus plugin"
+builder.metadata.description = "Simple pw_Si2 job submission with the aiida_abacus plugin"
 
 parameters = Dict(dict=all_parameters)
-# Run the calculation & print results
+# Run the calculation with parameters & print results
 results, node = engine.run.get_node(builder, parameters=parameters)
 misc = results["misc"].get_dict()
 print(f"Miscellaneous: {misc}")
+print(f"Total energy is: {misc['total_energy']} eV")
 retrieved = results["retrieved"]
 print(f"Retrieved files: {retrieved.list_object_names()}")
-remote_folder = results["remote_folder"].entry_point  # .get_remote_path()
-print(f"Remote folder entry_point: {remote_folder}")
+remote_folder_path = results["remote_folder"].get_remote_path()
+print(f"Remote folder path: {remote_folder_path}")
 
-print("Calc launch over.")
+print("Calculation over.")
+print("""
+You can use
+    `verdi process list -a`
+to see the status of the calculation.""")
